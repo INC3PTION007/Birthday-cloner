c78d2ac1094472e61ce59c281cb94d19e5ba80cc
Md.Rabby <91461469+inceptionff@users.noreply.github.com>
2022-10-25 02:34:40 +0600

    Add files via upload

---
+++ info.py
@@ -0,0 +1,34 @@
+import os
+os.system("clear")
+G="\033[1;32m"
+B="\033[1;34m"
+Y="\033[1;33m"
+P="\033[1;35m"
+C="\033[1;36m"
+
+design="""
+
+  INCEPTION
+                                            
+""".format(P=P,C=C,G=G,Y=Y)                                             
+
+
+logo=("""{B}
+**************************************************
+{G}        •••Allah Always Watching Me•••
+{B}**************************************************
+{C} ____You can't explain me properly____
+{Y}         ____Like INCEPTION____
+{B}**************************************************
+{ascii}
+{B}**************************************************
+{G}Admin    >>  Md.Rabbi [INCEPTION]
+{Y}Facebook >>  https://facebook.com/iam.inception
+{C}Github   >>  https://github.com/INC3PTION
+Tool V.  >>  0.2(DEBUG)
+{B}**************************************************
+""".format(ascii=design,B=B,C=C,G=G,Y=Y))
+
+User="RABBI"
+Pwd="whoami"
+Permission="On"
